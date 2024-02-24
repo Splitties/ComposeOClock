bc5a7100d620f909d766fd95ac054bd349eebc27
Yuri Schimke <yuri@schimke.ee>
2024-02-24 17:52:04 +0000

    Fix workflow

--- .github/workflows/build_workflow.main.kts
+++ .github/workflows/build_workflow.main.kts
@@ -54,15 +54,25 @@ workflow(
         )
         uses(
             name = "Upload test reports",
-            action = CustomAction(
-                actionOwner = "mikepenz",
-                actionName = "action-junit-report",
-                actionVersion = "v4",
-                inputs = mapOf(
-                    "report_paths" to "**/build/test-results/**/TEST-*.xml",
-                )
+            action = UploadArtifactV4(
+                name = "Junit",
+                path = listOf("**/build/test-results"),
+                retentionDays = UploadArtifactV4.RetentionPeriod.Default,
             ),
-            `if` = expr { failure() }
+            `if` = expr { always() }
         )
+        // TODO enable on main
+//        uses(
+//            name = "Upload test reports",
+//            action = CustomAction(
+//                actionOwner = "mikepenz",
+//                actionName = "action-junit-report",
+//                actionVersion = "v4",
+//                inputs = mapOf(
+//                    "report_paths" to "**/build/test-results/**/TEST-*.xml",
+//                )
+//            ),
+//            `if` = expr { failure() }
+//        )
     }
 }.writeToFile()

a95a16419c0df890e8941c8834d81a9d934bebae
Louis CAD <louis.cognault@gmail.com>
2024-06-21 02:39:30 +0200

    Comment out LoopingSeconds and LightLinesWatchFace in allWatchFaces
    That's especially important since LoopingSeconds is a battery drainer as it keeps updating even in ambient mode

--- shared/src/main/kotlin/watchfaces/AllWatchFaces.kt
+++ shared/src/main/kotlin/watchfaces/AllWatchFaces.kt
@@ -1,11 +1,10 @@
 package org.splitties.compose.oclock.sample.watchfaces
 
 import androidx.compose.runtime.Composable
-import org.splitties.compose.oclock.sample.elements.LoopingSeconds
 
 val allWatchFaces: List<@Composable () -> Unit> = listOf(
-    { LoopingSeconds() },
-    { LightLinesWatchFace() },
+//    { LoopingSeconds() },
+//    { LightLinesWatchFace() },
     { KotlinFanClock() },
     { ComposeFanClock() },
     { BasicAnalogClock() },
